69d925ae9ed0c622f005de18cb4a5a5098037d9a
yeonji <yeonji.qp@gmail.com>
2023-04-08 15:30:27 +0900

    feat: switch를 이용한 페이지 전환

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
 import { useState } from 'react';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const onSubmit = () => {
-    console.log(title, body);
+    axios.post('http://localhost:3001/posts', {
+      title,
+      body
+    })
   };
 
   return (
-    <div className="container">
-      <div className="mb-3">
-      <label className="form-label">Title</label>
-      <input
-        className="form-control"
-        value={title}
-        onChange={(event)=> {
-          setTitle(event.target.value);
-        }}
-      />
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/blogs">Blogs</Link>
       </div>
-      <div className="mb-3">
-        <label className="form-label">Body</label>
-        <textarea
-          className="form-control"
-          value={body}
-          onChange={(event) => {
-            setBody(event.target.value);
-          }}
-          rows="20"
-        />
-      </div>
-      <button className="btn btn-primary" onClick={onSubmit}>
-        Post
-      </button>
-    </div>
+      <Switch>
+        <Route path="/" exact>
+          Homepage
+        </Route>
+        <Route path="/blogs" exact>
+          <div className="container">
+            <div className="mb-3">
+              <label className="form-label">Title</label>
+              <input
+                className="form-control"
+                value={title}
+                onChange={(event)=> {
+                  setTitle(event.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Body</label>
+              <textarea
+                className="form-control"
+                value={body}
+                onChange={(event) => {
+                  setBody(event.target.value);
+                }}
+                rows="20"
+              />
+            </div>
+            <button className="btn btn-primary" onClick={onSubmit}>
+              Post
+            </button>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
