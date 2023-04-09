a2119c052d91de897d26ea4195323f680b4e8235
yeonji <yeonji.qp@gmail.com>
2023-04-09 15:01:27 +0900

    feat: 새로운 페이지 map으로 랜더링

--- src/App.js
+++ src/App.js
@@ -1,61 +1,22 @@
-import { useState } from 'react';
-import axios from 'axios';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
-} from 'react-router-dom'
+} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import routes from './routes';
 
 function App() {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const onSubmit = () => {
-    axios.post('http://localhost:3001/posts', {
-      title,
-      body
-    })
-  };
-
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/blogs">Blogs</Link>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          {routes.map((route) => {
+            return <Route exact path={route.path} component={route.component} key={route.path} />
+          })}
+        </Switch>
       </div>
-      <Switch>
-        <Route path="/" exact>
-          Homepage
-        </Route>
-        <Route path="/blogs" exact>
-          <div className="container">
-            <div className="mb-3">
-              <label className="form-label">Title</label>
-              <input
-                className="form-control"
-                value={title}
-                onChange={(event)=> {
-                  setTitle(event.target.value);
-                }}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Body</label>
-              <textarea
-                className="form-control"
-                value={body}
-                onChange={(event) => {
-                  setBody(event.target.value);
-                }}
-                rows="20"
-              />
-            </div>
-            <button className="btn btn-primary" onClick={onSubmit}>
-              Post
-            </button>
-          </div>
-        </Route>
-      </Switch>
     </Router>
   );
 }
