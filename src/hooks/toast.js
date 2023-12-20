964d008c87a7bc589caf0144380b4d48baeedd27
yeonji <yeonji.qp@gmail.com>
2023-12-20 22:28:10 +0900

    fix error

--- src/hooks/toast.js
+++ src/hooks/toast.js
@@ -1,21 +1,13 @@
 // 커스텀 hooks에서는 useState, useRef를 가져와 쓸 수 있다.
-import { useState, useRef } from "react"
 import { v4 as uuidv4 } from 'uuid';
-import { addToast as add } from '../store/toastSlice';
+import { addToast as add, removeToast } from '../store/toastSlice';
 import { useDispatch } from 'react-redux';
 
 const useToast = () => { // use를 앞에 붙여 훅이라는 것을 암시.
-  const toasts = useRef([]);
-  const [, setToastRerender] = useState(false);
   const dispatch = useDispatch(); // action을 dispatch를 통해 보낸다.
 
   const deleteToast = (id) => {
-    const filteredToasts = toasts.current.filter(toast => {
-      return toast.id !== id
-    })
-
-    toasts.current = filteredToasts;
-    setToastRerender(prev => !prev);
+    dispatch(removeToast(id));
   }
   
   const addToast = (toast) => {
@@ -26,19 +18,16 @@ const useToast = () => { // use를 앞에 붙여 훅이라는 것을 암시.
     }
 
     dispatch(add(toastWidthId));
-    // toasts.current = [...toasts.current, toastWidthId];
-    // setToastRerender(prev => !prev);
 
     setTimeout(()=>{
-      deleteToast(id, toasts, setToastRerender);
+      deleteToast(id);
     }, 5000)
   };
 
-  return [
-    toasts.current,
+  return {
     addToast,
     deleteToast
-  ]
+  }
 }
 
 export default useToast;
--- src/store/toastSlice.js
+++ src/store/toastSlice.js
@@ -9,6 +9,11 @@ const toastSlice = createSlice({
   reducers: { // 함수를 업데이트 하는 함수: 여기서만 state를 변경해줘야 함.
     addToast: (state, action) => {
       state.toasts.push(action.payload);
+    },
+    removeToast: (state, action) => {
+      state.toasts = state.toasts.filter(toast => {
+        return toast.id !== action.payload
+      })
     }
   }
 })
@@ -16,6 +21,6 @@ const toastSlice = createSlice({
 // console.log(toastSlice.actions.addToast()); // payload와 type을 가진 객체가 만들어진다.
 // console.log(toastSlice.actions.addToast('hello')); // payload에 hello들어감.
 
-export const { addToast } = toastSlice.actions;
+export const { addToast, removeToast } = toastSlice.actions;
 
 export default toastSlice.reducer;
