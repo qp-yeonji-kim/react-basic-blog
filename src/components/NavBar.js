a2e61c8372345747d4ae00607115c49d0002e40b
yeonji <yeonji.qp@gmail.com>
2023-12-24 01:23:03 +0900

    74. auth redux slice

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,10 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
-
+import { login, logout } from "../store/authSlice";
 
 const NavBar = () => {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
   return (
     <nav className="navbar navbar-dark bg-dark">
       <div className="container">
@@ -14,6 +17,20 @@ const NavBar = () => {
             flexDirection: 'row'
           }}
         >
+          <li className="nav-item">
+            <button
+              className="text-white btn btn-link text-decoration-none"
+              onClick={() => {
+                if (isLoggedIn) {
+                  dispatch(logout());
+                } else {
+                  dispatch(login());
+                }
+              }}  
+            >
+              { isLoggedIn ? 'Logout' : 'Login'}
+            </button>
+          </li>
           <li className="nav-item">
             <NavLink
               activeClassName="active"
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import toastReducer from './toastSlice'
+import authReducer from './authSlice'
 
 const store = configureStore({
   reducer: {
-    toast: toastReducer
+    toast: toastReducer,
+    auth: authReducer
   }
 })
 
